9c5f4efd788db3b36ce3265dd3c21480350c26bc
Mitya <mityaprr@mail.ru>
2014-10-26 14:22:08 +0300

    r0

---
+++ E.java
@@ -0,0 +1,30 @@
+public class E implements A, B {
+
+    private String h = "init";
+
+    private double b = 100.500;
+
+    public int hh() {
+        return new java.util.Random().nextInt();
+    }
+
+    public Object pp() {
+        return this;
+    }
+
+    public long dd() {
+        return 100500;
+    }
+
+    public String kk() {
+        return "Yes";
+    }
+
+    public java.util.List<String> jj() {
+        return new java.util.LinkedList<String>();
+    }
+
+    public void bb() {
+        System.out.println(getClass().getName());
+    }
+}
---
+++ G.java
@@ -0,0 +1,22 @@
+public class G implements A {
+
+    private double a = 100.500;
+
+    private byte c = 1;
+
+    public int[] ii() {
+        return new int[]{0, 1, 2, 3, 4};
+    }
+
+    public Object rr() {
+        return null;
+    }
+
+    public long dd() {
+        return 99999;
+    }
+
+    public String kk() {
+        return "Yes";
+    }
+}
